12d7436f800a7ed0ceffa90606e9e69765af38d4
VoGPro <krasnodar.kan@gmail.com>
2024-09-29 21:03:33 +0300

    4-5. Методы валидации необходимых полей

--- OOP/src/Car.java
+++ OOP/src/Car.java
@@ -1,3 +1,5 @@
+import java.time.Year;
+
 public class Car {
     private final int carId;
     private final String brand;
@@ -6,12 +8,69 @@ public class Car {
     private final double price;
     private final String type;
 
-    private Car(int carId, String brand, String model, int year, double price, String type) {
+    public Car(int carId, String brand, String model, int year, double price, String type) {
+        validateCarId(carId);
+        validateYear(year);
+        validatePrice(price);
         this.carId = carId;
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.price = price;
         this.type = type;
+    }
+
+    private static void validateCarId(int carId) {
+        if (carId <= 0) {
+            throw new IllegalArgumentException("ID должно быть целым положительным числом");
+        }
+    }
+
+    private static void validateYear(int year) {
+        if (year < 1886 || year > Year.now().getValue()) {
+            throw new IllegalArgumentException("Год выпуска автомобиля не может быть " + year);
+        }
+    }
+
+    private static void validatePrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Цена не может быть меньше или равна нулю");
+        }
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                "carId: " + carId +
+                ", brand: " + brand +
+                ", model: " + model +
+                ", year: " + year +
+                ", price: " + price +
+                ", type: " + type +
+                '}';
     }
 }
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Car car = new Car(1, "Nissan", "R34", 2002, 320, "Sport");
+        System.out.printf(car.toString());
     }
 }
