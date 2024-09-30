d906cae486a5be474d17492dec571d1c00b18106
VoGPro <krasnodar.kan@gmail.com>
2024-09-30 13:28:01 +0300

    7. Вывод полной и краткой версии объекта, сравнение объектов

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Car car = Car.createFromString("1, Nissan, R34, 2002, 35209.4, Sport");
-        System.out.printf(car.toString());
+        System.out.println("Краткая версия: " + car.shortVersion());
+        System.out.println("Полная версия: " + car.toString());
+
+        Car car1 = Car.createCar(1, "Nissan", "R34", 2002, 35209.4, "Sport");
+        Car car2 = Car.createCar(2, "Mazda", "RX-7", 2002, 37208.3, "Sport");
+
+        if (car1.equals(car)) {
+            System.out.println("car1 равен car");
+        } else {
+            System.out.println("car1 не равен car");
+        }
+
+        if (car1.equals(car2)) {
+            System.out.println("car1 равен car2");
+        } else {
+            System.out.println("car1 не равен car2");
+        }
     }
 }
