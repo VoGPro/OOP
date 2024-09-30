2e74fb1dbf44e2cd9b763500ae8a0d6136e8c8cb
VoGPro <krasnodar.kan@gmail.com>
2024-09-30 13:49:11 +0300

    8. Создание краткого класса

---
+++ OOP/src/BriefCar.java
@@ -0,0 +1,67 @@
+import java.time.Year;
+import java.util.Objects;
+
+public class BriefCar {
+    private final int carId;
+    private final String brand;
+    private final String model;
+
+    public BriefCar(int carId, String brand, String model) {
+        validateCarId(carId);
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public static BriefCar createFromString(String carString) {
+        String[] parts = carString.split(",");
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Неверный тип данных. Ожидается строка, разделённая 3 запятыми.");
+        }
+        try {
+            int carId = Integer.parseInt(parts[0].trim());
+            String brand = parts[1].trim();
+            String model = parts[2].trim();
+            return new BriefCar(carId, brand, model);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Неверный формат данных в строке", e);
+        }
+    }
+
+    private static void validateCarId(int carId) {
+        if (carId <= 0) {
+            throw new IllegalArgumentException("ID должно быть целым положительным числом");
+        }
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                "carId: " + carId +
+                ", brand: " + brand +
+                ", model: " + model +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BriefCar car = (BriefCar) o;
+        return carId == car.carId &&
+                Objects.equals(brand, car.brand) &&
+                Objects.equals(model, car.model);
+    }
+}
