6f635157b9e58d4df12c41640047a700b160b3c1
VoGPro <112944896+VoGPro@users.noreply.github.com>
2024-11-03 20:22:10 +0300

    Delete OOP/src directory

--- OOP/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Car car0 = new Car(1, "1hgbh41jxmn109186", "Nissan", "R34", 2002, 35209.4, "Sport");
-        Car car1 = Car.createFromString("1, 1hgbh41jxmn109186, Nissan, R34, 2002, 35209.4, Sport");
-    }
-}
